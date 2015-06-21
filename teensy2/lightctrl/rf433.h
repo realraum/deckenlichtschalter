29e9b0751143c88e0df805617bb2917a1e8ad110
Christian Pointner <equinox@spreadspace.org>
2015-06-21 19:12:30 +0200

    fixed commit

---
+++ teensy2/lightctrl/rf433.h
@@ -0,0 +1,30 @@
+/*
+ *  rf433 power outlet control Teensy Controller Code
+ *
+ *
+ *  Copyright (C) 2013 Bernhard Tittelbach <xro@realraum.at>
+ *   uses avr-utils, anyio & co by Christian Pointner <equinox@spreadspace.org>
+ *
+ *  This file is part of spreadspace avr utils.
+ *
+ *  spreadspace avr utils is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  spreadspace avr utils is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with spreadspace avr utils. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIGHTCTRL_rf423_h_INCLUDED
+#define LIGHTCTRL_rf433_h_INCLUDED
+
+void rf433_send_rf_cmd(uint8_t sr[]);
+void rf433_check_frame_done(void);
+
+#endif
