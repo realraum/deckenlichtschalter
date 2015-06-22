c37b90fed1c80910e843418d5e432eacddf6bc63
Christian Pointner <equinox@spreadspace.org>
2015-06-22 22:52:55 +0200

    fixed copyright header

--- teensy2/lightctrl/rf433.c
+++ teensy2/lightctrl/rf433.c
@@ -1,24 +1,24 @@
 /*
- *  rf433 power outlet control Teensy Controller Code
+ *  lightctrl - teensy firmware code
  *
  *
  *  Copyright (C) 2013 Bernhard Tittelbach <xro@realraum.at>
  *   uses avr-utils, anyio & co by Christian Pointner <equinox@spreadspace.org>
  *
- *  This file is part of spreadspace avr utils.
+ *  This file is part of lightctrl.
  *
- *  spreadspace avr utils is free software: you can redistribute it and/or modify
+ *  lightctrl is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  any later version.
  *
- *  spreadspace avr utils is distributed in the hope that it will be useful,
+ *  lightctrl is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with spreadspace avr utils. If not, see <http://www.gnu.org/licenses/>.
+ *  along with lightctrl. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "mypins.h"
