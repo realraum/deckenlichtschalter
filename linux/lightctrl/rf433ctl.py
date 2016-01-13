1d20954ff9da1e371f520e88571a3e121c66d862
Bernhard Tittelbach <bernhard@tittelbach.org>
2016-01-13 08:06:48 +0100

    object oriented

--- linux/lightctrl/rf433ctl.py
+++ linux/lightctrl/rf433ctl.py
@@ -5,113 +5,133 @@ import sys,os
 import json
 import time
 import paho.mqtt.client as mqtt
+import Pyro4
 
-ttydev="/dev/ttyACM0"
+class SwitchARealSwitch():
+    def __init__(self, ttydev, mqtthost, mqttport):
+        self.ttydev = ttydev
+        self.mqtthost = mqtthost
+        self.mqttport = mqttport
+        self.ttyfh=None
+        self.mqttcon=None
 
-ttyfh=None
-def localtty(code):
-    global ttyfh
-    if ttyfh is None:
-      ttyfh=open(ttydev,"wb")
-    ttyfh.write(b">"+code)
-    ttyfh.flush()
+        self.rfcodes = {
+          "regalleinwand":{"on":b"\xa2\xa0\xa8","off":b"\xa2\xa0\x28", "transmitter":self.rfcode2TTY}, #white remote B 1
+          "bluebar":{"on":b"\xa8\xa0\xa8","off":b"\xa8\xa0\x28", "transmitter":self.rfcode2TTY}, #white remote C 1
+          "labortisch":{"on":b"\xa2\xa2\xaa","off":b"\xa2\xa2\x2a", "transmitter":self.rfcode2TTY},
+          "couchred":{"on":b"\x8a\xa0\x8a","off":b"\x8a\xa0\x2a", "transmitter":self.rfcode2TTY}, #pollin 00101 a
+          "couchwhite":{"on":b"\x8a\xa8\x88","off":b"\x8a\xa8\x28", "transmitter":self.rfcode2TTY}, #pollin 00101 d
+          "cxleds":{"on":b"\x8a\x88\x8a","off":b"\x8a\x88\x2a", "transmitter":self.rfcode2TTY}, #pollin 00101 b
+          "mashadecke":{"on":b"\x8a\x28\x8a","off":b"\x8a\x28\x2a", "transmitter":self.rfcode2TTY}, #pollin 00101 c
+          "boiler":{"on":b"\xa0\xa2\xa8","off":b"\xa0\xa2\x28", "transmitter":self.rfcode2BOTH}, #white remote A 2
+          "spots":{"on":b"\x00\xaa\x88","off":b"\x00\xaa\x28", "transmitter":self.rfcode2TTY}, #polling 11110 d
+          "lichtpi":{"on":b"\x00\xa2\x8a","off":b"\x00\xa2\x2a", "transmitter":self.rfcode2TTY}, # Funksteckdose an welcher der RaspberryPi / Kiosk / Deckenlicht hängt
+          "abwasch":{"on":b"\xaa\xa2\xa8","off":b"\xaa\xa2\x28", "transmitter":self.rfcode2MQTT},  #alte jk16 decke vorne
 
-mqttc=None
-def mqtt_connect():
-  global mqttc
-  if mqttc is None:
-    mqttc = mqtt.Client(client_id="rf433ctl@licht")
-    mqttc.connect("mqtt.realraum.at", 1883, 60)
+          "ymhpoweroff":{"on":"ymhpoweroff", "transmitter":self.ymhircmd2MQTT},
+          "ymhpower":{"on":"ymhpower", "off":"ymhpoweroff", "transmitter":self.ymhircmd2MQTT},
+          "ymhpoweron":{"on":"ymhpoweron", "transmitter":self.ymhircmd2MQTT},
+          "ymhcd":{"on":"ymhcd", "transmitter":self.ymhircmd2MQTT},
+          "ymhtuner":{"on":"ymhtuner", "transmitter":self.ymhircmd2MQTT},
+          "ymhtape":{"on":"ymhtape", "transmitter":self.ymhircmd2MQTT},
+          "ymhwdtv":{"on":"ymhwdtv", "transmitter":self.ymhircmd2MQTT},
+          "ymhsattv":{"on":"ymhsattv", "transmitter":self.ymhircmd2MQTT},
+          "ymhvcr":{"on":"ymhvcr", "transmitter":self.ymhircmd2MQTT},
+          "ymh7":{"on":"ymh7", "transmitter":self.ymhircmd2MQTT},
+          "ymhaux":{"on":"ymhaux", "transmitter":self.ymhircmd2MQTT},
+          "ymhextdec":{"on":"ymhextdec", "transmitter":self.ymhircmd2MQTT},
+          "ymhtest":{"on":"ymhtest", "transmitter":self.ymhircmd2MQTT},
+          "ymhtunabcde":{"on":"ymhtunabcde", "transmitter":self.ymhircmd2MQTT},
+          "ymheffect":{"on":"ymheffect", "transmitter":self.ymhircmd2MQTT},
+          "ymhtunplus":{"on":"ymhtunplus", "transmitter":self.ymhircmd2MQTT},
+          "ymhtunminus":{"on":"ymhtunminus", "transmitter":self.ymhircmd2MQTT},
+          "ymhvolup":{"on":"ymhvolup", "transmitter":self.ymhircmd2MQTT},
+          "ymhvoldown":{"on":"ymhvoldown", "transmitter":self.ymhircmd2MQTT},
+          "ymhvolmute":{"on":"ymhvolmute", "transmitter":self.ymhircmd2MQTT},
+          "ymhmenu":{"on":"ymhmenu", "transmitter":self.ymhircmd2MQTT},
+          "ymhplus":{"on":"ymhplus", "transmitter":self.ymhircmd2MQTT},
+          "ymhminus":{"on":"ymhminus", "transmitter":self.ymhircmd2MQTT},
+          "ymhtimelevel":{"on":"ymhtimelevel", "transmitter":self.ymhircmd2MQTT},
+          "ymhprgdown":{"on":"ymhprgdown", "transmitter":self.ymhircmd2MQTT},
+          "ymhprgup":{"on":"ymhprgup", "transmitter":self.ymhircmd2MQTT},
+          "ymhsleep":{"on":"ymhsleep", "transmitter":self.ymhircmd2MQTT},
+          "ymhp5":{"on":"ymhp5", "transmitter":self.ymhircmd2MQTT},
+        }
+        #  "jk16decke":{"on:":"\xaa\xa0\xa8","off":"\xaa\xa0\x28"},
 
-def mqttsendcode(code, presend_delay_ms=0):
-  mqtt_connect()
-  if presend_delay_ms > 0:
-    mqttc.publish("action/rf433/setdelay", json.dumps({"Location":"pillar", "DelayMs":presend_delay}, retain=True),qos=1)
-  mqttc.publish("action/rf433/sendcode3byte", json.dumps({"Code":list(code), "Ts":time.time()}),qos=1)
+    def tty(self):
+        if self.ttyfh is None:
+            self.ttyfh=open(self.ttydev,"wb")
+        return self.ttyfh
 
-def mqttsendymhircmd(cmd):
-  mqtt_connect()
-  mqttc.publish("action/yamahastereo/ircmd", json.dumps({"Cmd":cmd, "Ts":time.time()}),qos=1)
+    def mqttc(self):
+        if self.mqttcon is None:
+            self.mqttcon = mqtt.Client(client_id="rf433ctl@licht")
+            self.mqttcon.connect(self.mqtthost, self.mqttport, 60)
+            self.mqttcon.loop_start()
+        return self.mqttcon
+
+    def cleanup(self):
+        if not self.ttyfh is None:
+            self.ttyfh.close()
+        if not self.mqttcon is None:
+            self.mqttcon.loop_stop()
+            self.mqttcon.disconnect()
+
+    def rfcode2TTY(self, code):
+        self.tty().write(b">"+code)
+        self.tty().flush()
+
+    def rfcode2MQTT(self, code, presend_delay_ms=0):
+        if presend_delay_ms > 0:
+            self.mqttc().publish("action/rf433/setdelay", json.dumps({"Location":"pillar", "DelayMs":presend_delay}, retain=True),qos=1)
+        self.mqttc().publish("action/rf433/sendcode3byte", json.dumps({"Code":list(code), "Ts":time.time()}),qos=1)
+
+    def ymhircmd2MQTT(self, cmd):
+        self.mqttc().publish("action/yamahastereo/ircmd", json.dumps({"Cmd":cmd, "Ts":time.time()}),qos=1)
+
+    def rfcode2BOTH(self, code):
+        self.rfcode2TTY(code)
+        time.sleep(2000)
+        self.rfcode2MQTT(code)
+
+    def toggleSwitch(self, onoff, rfname):
+        try:
+            code = self.rfcodes[rfname][onoff]
+        except KeyError:
+            return
+        try:
+            self.rfcodes[rfname]["transmitter"](code)
+        except KeyError:
+            self.rfcode2TTY(code)
 
 
-def both(code):
-  localtty(code)
-  #mqttsendcode(code,2000)
-  time.sleep(2)
-  mqttsendcode(code)
+class MultiSwitcherQueue():
+    def __init__(self, switcher):
+        self.switcher=switcher
 
-rfcodes = {
-  "regalleinwand":{"on":b"\xa2\xa0\xa8","off":b"\xa2\xa0\x28", "transmitter":localtty}, #white remote B 1
-  "bluebar":{"on":b"\xa8\xa0\xa8","off":b"\xa8\xa0\x28", "transmitter":localtty}, #white remote C 1
-  "labortisch":{"on":b"\xa2\xa2\xaa","off":b"\xa2\xa2\x2a", "transmitter":localtty},
-  "couchred":{"on":b"\x8a\xa0\x8a","off":b"\x8a\xa0\x2a", "transmitter":localtty}, #pollin 00101 a
-  "couchwhite":{"on":b"\x8a\xa8\x88","off":b"\x8a\xa8\x28", "transmitter":localtty}, #pollin 00101 d
-  "cxleds":{"on":b"\x8a\x88\x8a","off":b"\x8a\x88\x2a", "transmitter":localtty}, #pollin 00101 b
-  "mashadecke":{"on":b"\x8a\x28\x8a","off":b"\x8a\x28\x2a", "transmitter":localtty}, #pollin 00101 c
-  "boiler":{"on":b"\xa0\xa2\xa8","off":b"\xa0\xa2\x28", "transmitter":both}, #white remote A 2
-  "spots":{"on":b"\x00\xaa\x88","off":b"\x00\xaa\x28", "transmitter":localtty}, #polling 11110 d
-  "lichtpi":{"on":b"\x00\xa2\x8a","off":b"\x00\xa2\x2a", "transmitter":localtty}, # Funksteckdose an welcher der RaspberryPi / Kiosk / Deckenlicht hängt
-  "abwasch":{"on":b"\xaa\xa2\xa8","off":b"\xaa\xa2\x28", "transmitter":mqttsendcode},  #alte jk16 decke vorne
+        self.multinames = {
+          "ambientlights":["bluebar","couchred","couchwhite","regalleinwand","cxleds","abwasch"],
+          "all":list(set(switcher.rfcodes.keys()) - set(["lichtpi"]))
+        }
 
-  "ymhpoweroff":{"on":"ymhpoweroff", "transmitter":mqttsendymhircmd},
-  "ymhpower":{"on":"ymhpower", "off":"ymhpoweroff", "transmitter":mqttsendymhircmd},
-  "ymhpoweron":{"on":"ymhpoweron", "transmitter":mqttsendymhircmd},
-  "ymhcd":{"on":"ymhcd", "transmitter":mqttsendymhircmd},
-  "ymhtuner":{"on":"ymhtuner", "transmitter":mqttsendymhircmd},
-  "ymhtape":{"on":"ymhtape", "transmitter":mqttsendymhircmd},
-  "ymhwdtv":{"on":"ymhwdtv", "transmitter":mqttsendymhircmd},
-  "ymhsattv":{"on":"ymhsattv", "transmitter":mqttsendymhircmd},
-  "ymhvcr":{"on":"ymhvcr", "transmitter":mqttsendymhircmd},
-  "ymh7":{"on":"ymh7", "transmitter":mqttsendymhircmd},
-  "ymhaux":{"on":"ymhaux", "transmitter":mqttsendymhircmd},
-  "ymhextdec":{"on":"ymhextdec", "transmitter":mqttsendymhircmd},
-  "ymhtest":{"on":"ymhtest", "transmitter":mqttsendymhircmd},
-  "ymhtunabcde":{"on":"ymhtunabcde", "transmitter":mqttsendymhircmd},
-  "ymheffect":{"on":"ymheffect", "transmitter":mqttsendymhircmd},
-  "ymhtunplus":{"on":"ymhtunplus", "transmitter":mqttsendymhircmd},
-  "ymhtunminus":{"on":"ymhtunminus", "transmitter":mqttsendymhircmd},
-  "ymhvolup":{"on":"ymhvolup", "transmitter":mqttsendymhircmd},
-  "ymhvoldown":{"on":"ymhvoldown", "transmitter":mqttsendymhircmd},
-  "ymhvolmute":{"on":"ymhvolmute", "transmitter":mqttsendymhircmd},
-  "ymhmenu":{"on":"ymhmenu", "transmitter":mqttsendymhircmd},
-  "ymhplus":{"on":"ymhplus", "transmitter":mqttsendymhircmd},
-  "ymhminus":{"on":"ymhminus", "transmitter":mqttsendymhircmd},
-  "ymhtimelevel":{"on":"ymhtimelevel", "transmitter":mqttsendymhircmd},
-  "ymhprgdown":{"on":"ymhprgdown", "transmitter":mqttsendymhircmd},
-  "ymhprgup":{"on":"ymhprgup", "transmitter":mqttsendymhircmd},
-  "ymhsleep":{"on":"ymhsleep", "transmitter":mqttsendymhircmd},
-  "ymhp5":{"on":"ymhp5", "transmitter":mqttsendymhircmd},
-}
-#  "jk16decke":{"on:":"\xaa\xa0\xa8","off":"\xaa\xa0\x28"},
+    def toggleSwitch(self, onoff, rfname):
+        if rfname in self.multinames:
+            for rfname2 in self.multinames[rfname]:
+                self.switcher.toggleSwitch(onoff, rfname2)
+        else:
+            self.switcher.toggleSwitch(onoff, rfname)
 
-multinames = {
-  "ambientlights":["bluebar","couchred","couchwhite","regalleinwand","cxleds","abwasch"],
-  "all":list(set(rfcodes.keys()) - set(["lichtpi"]))
-}
 
-namestoswitch=[]
+
+switcher = SwitchARealSwitch("/dev/ttyACM0","mqtt.realraum.at",1883)
+multiswitcher = MultiSwitcherQueue(switcher)
+
 
 if len(sys.argv) > 2:
-  if sys.argv[1] == "1":
-    sys.argv[1] = "on"
-  elif sys.argv[1] == "0":
-    sys.argv[1] = "off"
-  if sys.argv[2] in multinames:
-    namestoswitch = multinames[sys.argv[2]]
-  elif sys.argv[2] in rfcodes:
-    namestoswitch = [sys.argv[2]]
-  else:
-    sys.exit(1)
-  for rfname in namestoswitch:
-    if sys.argv[1] in rfcodes[rfname]:
-      code = rfcodes[rfname][sys.argv[1]]
-      try:
-        rfcodes[rfname]["transmitter"](code)
-      except KeyError:
-        localtty(code)
-
-if not ttyfh is None:
-  ttyfh.close()
-if not mqttc is None:
-  mqttc.loop()
-  mqttc.disconnect()
+    if sys.argv[1] == "1":
+        sys.argv[1] = "on"
+    elif sys.argv[1] == "0":
+        sys.argv[1] = "off"
+    multiswitcher.toggleSwitch(sys.argv[1], sys.argv[2])
+    switcher.cleanup()
