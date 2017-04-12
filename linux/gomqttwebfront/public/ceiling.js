0b12d932c18b2bcea74bfd7097beb3c22f1a513c
Bernhard Tittelbach <bernhard@tittelbach.org>
2017-04-12 15:58:07 +0200

    move some common mqtt to own file

--- linux/gomqttwebfront/public/ceiling.js
+++ linux/gomqttwebfront/public/ceiling.js
@@ -37,35 +37,6 @@ function setButtonStates(data) {
   renderButtonStates();
 }
 
-function sendMQTT_XHTTP(ctx, data) {
-  var req = new XMLHttpRequest;
-  req.open("POST", cgiUrl, true);
-  req.onload = function() {
-    if (req.status != 200) {
-      return;
-    }
-    var data = JSON.parse(req.responseText);
-    setButtonStates(data);
-  };
-  var param = "Ctx=" + encodeURIComponent(ctx);
-  params = params + "&Data="+encodeURIComponent(data);
-  params = params.replace(/%20/g, '+');
-  req.overrideMimeType("application/json");
-  req.setRequestHeader("googlechromefix","");
-  req.setRequestHeader("Content-length", params.length);
-  req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  req.setRequestHeader("Connection", "close");
-  req.send(params);
-}
-
-function sendMQTT(ctx, data) {
-  if (webSocketSupport) {
-    ws.send(ctx,data);
-  } else {
-    sendMQTT_XHTTP(ctx, data);
-  }
-}
-
 var webSocketUrl = 'ws://'+window.location.hostname+'/sock';
 var cgiUrl = '/cgi-bin/fallback.cgi';
 
--- linux/gomqttwebfront/public/switch.js
+++ linux/gomqttwebfront/public/switch.js
@@ -157,35 +157,6 @@ function popupFancyColorPicker(event) {
 }
 
 
-function sendMQTT_XHTTP(ctx, data) {
-  var req = new XMLHttpRequest;
-  req.open("POST", cgiUrl, true);
-  req.onload = function() {
-    if (req.status != 200) {
-      return;
-    }
-    var data = JSON.parse(req.responseText);
-    setButtonStates(data);
-  };
-  var param = "Ctx=" + encodeURIComponent(ctx);
-  params = params + "&Data="+encodeURIComponent(JSON.stringify(data));
-  params = params.replace(/%20/g, '+');
-  req.overrideMimeType("application/json");
-  req.setRequestHeader("googlechromefix","");
-  req.setRequestHeader("Content-length", params.length);
-  req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  req.setRequestHeader("Connection", "close");
-  req.send(params);
-}
-
-function sendMQTT(ctx, data) {
-  if (webSocketSupport) {
-    ws.send(ctx,data);
-  } else {
-    sendMQTT_XHTTP(ctx, data);
-  }
-}
-
 function setLedPipePattern(data) {
   sendMQTT("action/PipeLEDs/pattern",data);
 }
