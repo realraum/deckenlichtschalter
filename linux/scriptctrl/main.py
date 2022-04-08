5a79ac7b0d676c658d13956688083828de6c3cf8
Bernhard Tittelbach <bernhard@tittelbach.org>
2022-04-08 18:59:51 +0200

    no significant change

--- linux/scriptctrl/main.py
+++ linux/scriptctrl/main.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-from interface import CeilingClass, myclientid
 import importlib
 import traceback
+from interface import CeilingClass
 
-def loadScripts(ceiling):
+def loadScripts(ceilingobj):
     scripts = importlib.import_module("scripts.__init__")
     for module_name in scripts.__all__:
         try:
-            foo = importlib.import_module("scripts."+module_name)
-            scr = ceiling.newScript(module_name)
-            foo.init(scr)
+            imported_module = importlib.import_module("scripts."+module_name)
+            scr = ceilingobj.newScript(module_name)
+            imported_module.init(scr)
         except:
             traceback.print_exc()
-            ceiling.removeScript(module_name)
+            ceilingobj.removeScript(module_name)
 
 
 if __name__ == "__main__":
