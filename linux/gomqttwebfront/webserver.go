023a67d61a987d220089e0b44582c3e09f11febd
Bernhard Tittelbach <bernhard@tittelbach.org>
2022-11-14 18:14:42 +0100

    add rgb-ledstrip of commanderred

--- linux/gomqttwebfront/webserver.go
+++ linux/gomqttwebfront/webserver.go
@@ -77,6 +77,7 @@ var (
 		"action/abwasch/light",
 		"action/flooddoor/light",
 		"action/funkbude/light",
+		"action/rgb-ledstrip/status",
 	}
 	topics_basic_ceiling = []string{
 		"action/GoLightCtrl/basiclight1",
